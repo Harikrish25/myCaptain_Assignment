ee2c2c89a2b72ce0cd7aad7d8f74f6b05ef5183b
Harikrishna S <90301998+HarikrishnaS25@users.noreply.github.com>
2021-09-08 00:37:11 -0700

    Update Fileextension.py

--- Fileextension.py
+++ Fileextension.py
@@ -1,1 +1,8 @@
-
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> filename = input("Input the Filename:")
+Input the Filename:abc.py
+>>> f_extns = filename.split(".")
+>>> print ("The extension of the file is:"+repr(f_extns[-1]))
+The extension of the file is:'py'
+>>>
