7e2655dba3a63b69fdc91f3a7a38086d3fef3fde
Harikrishna S <90301998+HarikrishnaS25@users.noreply.github.com>
2021-09-10 22:19:57 -0700

    Update Area.py

--- Area.py
+++ Area.py
@@ -1,9 +1,4 @@
-Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import math
->>> r = float(input("The radius of a circle:"))
-The radius of a circle:1.1
->>> area = math.pi*r**2
->>> print("Area of a circle with radius "+ str(r) +" is:%.16f" %area)
-Area of a circle with radius 1.1 is:3.8013271108436504
->>> 
+import math
+r = float(input("The radius of a circle:"))
+area = math.pi*r**2
+print("Area of a circle with radius "+ str(r) +" is:%.16f" %area) 
